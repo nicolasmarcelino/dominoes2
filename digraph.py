6c93cf8e88ee9af21991a036c8d47de4997fca77
Nicolas Marcelino <141439592+nicolasmarcelino@users.noreply.github.com>
2026-09-07 17:29:45 -0300

    update digraph

--- digraph.py
+++ digraph.py
@@ -11,29 +11,3 @@ class Digraph:
         v, w = int(v), int(w)
         self.adj[v].add(w)
         self.E += 1
-
-    def degree(self, v):
-        return self.adj[v].size()
-
-    def max_degree(self):
-        max_deg = 0
-        for v in range(self.V):
-            max_deg = max(max_deg, self.degree(v))
-        return max_deg
-
-    def number_of_self_loops(self):
-        count = 0
-        for v in range(self.V):
-            for w in self.adj[v]:
-                if w == v:
-                    count += 1
-        return count
-
-    def reverse(self):
-        R = Digraph(self.V)
-        v = 0
-        while v < self.V:
-            for w in self.adj[v]:
-                R.add_edge(w, v)
-            v += 1
-        return R
