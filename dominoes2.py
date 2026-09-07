404afb23f35648fa04378cd13028b0eb8ce15657
Nicolas Marcelino <141439592+nicolasmarcelino@users.noreply.github.com>
2026-09-07 14:19:25 -0300

    update dominoes2

--- dominoes2.py
+++ dominoes2.py
@@ -25,7 +25,7 @@ while testes:
     total_dominos, total_enfileirados, total_empurrados = input().split()
     total_dominos, total_enfileirados, total_empurrados = int(total_dominos), int(total_enfileirados), int(total_empurrados)
 
-    if not (1 <= total_dominos <= 10000 and 1 <= total_enfileirados <= 10000 and 1 <= total_empurrados <= 10000):
+    if not (0 <= total_dominos <= 10000 and 0 <= total_enfileirados <= 10000 and 0 <= total_empurrados <= 10000):
         testes -= 1
         continue
 
