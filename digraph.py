d8cd64a2dd5c3b7ff15be5cafec9eec0dcbe9c9c
Nicolas Marcelino <141439592+nicolasmarcelino@users.noreply.github.com>
2026-09-08 14:31:17 -0300

    replace bag with native Python lists

--- digraph.py
+++ digraph.py
@@ -1,13 +1,11 @@
-from bag import Bag
-
 class Digraph:
 
     def __init__(self, v=0):
         self.V = v
         self.E = 0
-        self.adj = [Bag() for _ in range(self.V)]
+        self.adj = [[] for _ in range(self.V)]
 
     def add_edge(self, v, w):
         v, w = int(v), int(w)
-        self.adj[v].add(w)
+        self.adj[v].append(w)
         self.E += 1
--- directed_dfs.py
+++ directed_dfs.py
@@ -4,6 +4,7 @@ class DirectedDFS:
 
     def __init__(self, G, sources):
         self._marked = [False for _ in range(G.V)]
+        self.count = 0
         for s in sources:
             s = int(s)
             if not self._marked[s]:
@@ -11,9 +12,7 @@ class DirectedDFS:
 
     def dfs(self, G, v):
         self._marked[v] = True
+        self.count += 1
         for w in G.adj[v]:
             if not self._marked[w]:
                 self.dfs(G, w)
-
-    def marked(self, v):
-        return self._marked[v]
