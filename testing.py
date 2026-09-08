56241a968e787ab31013ed775385c3c372c76469
Nicolas Marcelino <141439592+nicolasmarcelino@users.noreply.github.com>
2026-09-08 14:32:54 -0300

    update testing

--- testing.py
+++ testing.py
@@ -9,37 +9,7 @@ g.add_edge(2, 4)
 g.add_edge(3, 0)
 g.add_edge(4, 6)
 g.add_edge(5, 6)
-
+print(g.adj)
 reachable = DirectedDFS(g, [1])
-
-for v in range(g.V):
-    if reachable.marked(v):
-        ## adicionar +1 pra considerar de 1 a n
-        print(v, "is reachable from source(s)")
-
-derrubados = 0
-for v in range(g.V):
-    if reachable.marked(v):
-        derrubados += 1
-print("derrubados:", derrubados)
-
-'''
-print(derrubados)
-1 -> [2]
-2 -> [3, 4]
-3 -> [5]
-4 -> [1]
-5 -> [7]
-6 -> [7]
-7 -> []
-
-1
-7 7 1
-1 2
-2 4
-2 3
-3 5
-4 1
-5 7
-6 7
-'''
+print(reachable._marked)
+print(reachable.count)
